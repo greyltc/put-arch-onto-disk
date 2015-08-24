dcc44e2c9b51a7fc61c04612cdc5e6c89d5995a9
Grey Christoforo <grey@christoforo.net>
2015-08-24 09:29:07 +0100

    install only minimum packages

--- put-arch-onto-disk.sh
+++ put-arch-onto-disk.sh
@@ -32,7 +32,7 @@ THIS="$( cd "$(dirname "$0")" ; pwd -P )"/$(basename $0)
 : ${CLEAN_UP:=false}
 
 DEFAULT_PACKAGES="base grub efibootmgr btrfs-progs dosfstools exfat-utils f2fs-tools openssh gpart parted jfsutils mtools nilfs-utils ntfs-3g hfsprogs gdisk arch-install-scripts bash-completion reflector rsync"
-pacman -S $DEFAULT_PACKAGES
+pacman -Sy efibootmgr btrfs-progs dosfstools f2fs-tools gpart parted gdisk arch-install-scripts
 
 if [ -b $TARGET ] ; then
   TARGET_DEV=$TARGET
