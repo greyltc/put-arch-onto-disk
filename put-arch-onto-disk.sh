2e0d3cb3974c4687f500d6927973709ce510c402
Grey Christoforo <grey@christoforo.net>
2015-09-11 10:19:11 +0200

    fix stupid logic in reflect_mirrors

--- put-arch-onto-disk.sh
+++ put-arch-onto-disk.sh
@@ -112,8 +112,8 @@ cat > /usr/bin/reflect_mirrors <<END
 
 #This will run reflector on mirrorlist, copying from backup first, overwriting
 
-curl -o /etc/pacman.d/mirrorlist.backup https://www.archlinux.org/mirrorlist/all/
-cp /etc/pacman.d/mirrorlist.backup /etc/pacman.d/mirrorlist
+mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+curl -o /etc/pacman.d/mirrorlist https://www.archlinux.org/mirrorlist/all/
 reflector --verbose -l 200 -p http --sort rate --save /etc/pacman.d/mirrorlist
 END
 chown root:users /usr/bin/reflect_mirrors
