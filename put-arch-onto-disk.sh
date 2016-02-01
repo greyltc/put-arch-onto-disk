ad84db29336c61268148e8230a67c5a738d6e6d9
Grey Christoforo <grey@christoforo.net>
2016-02-01 13:58:08 +0000

    pacaur can't be run as root

--- put-arch-onto-disk.sh
+++ put-arch-onto-disk.sh
@@ -42,7 +42,7 @@ THIS="$( cd "$(dirname "$0")" ; pwd -P )"/$(basename $0)
 
 if [[ $TARGET_ARCH == *"arm"* ]]
 then
-  pacaur -Sy --needed --noconfirm qemu-user-static binfmt-support
+  su ${USER} -c 'pacaur -Sy --needed --noconfirm qemu-user-static binfmt-support'
   update-binfmts --enable qemu-arm
 else
   # alarm does not like/need these
