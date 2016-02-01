2f0f81b420c5cf1c5415d56f56b10ece0acb5fbe
Grey Christoforo <grey@christoforo.net>
2016-02-01 14:04:54 +0000

    Update put-arch-onto-disk.sh

--- put-arch-onto-disk.sh
+++ put-arch-onto-disk.sh
@@ -42,7 +42,7 @@ THIS="$( cd "$(dirname "$0")" ; pwd -P )"/$(basename $0)
 
 if [[ $TARGET_ARCH == *"arm"* ]]
 then
-  ehco "please ensure qemu-user-static and binfmt-support are installed from the AUR"
+  echo "Please ensure qemu-user-static and binfmt-support are installed from the AUR"
   #su ${USER} -c 'pacaur -Sy --needed --noconfirm qemu-user-static binfmt-support'
   update-binfmts --enable qemu-arm
 else
