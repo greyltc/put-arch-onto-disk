83c23b8f2155474199b7d98afa47aa8c08e6e1ad
Grey Christoforo <grey@christoforo.net>
2016-03-12 14:04:55 +0000

    syntax fix

--- put-arch-onto-disk.sh
+++ put-arch-onto-disk.sh
@@ -399,7 +399,8 @@ END
   
   # some retarded bioses are hardcoded to only boot from /boot/EFI/Boot/BOOTX64.EFI (looking at you Sony)
   if [ "$UEFI_COMPAT_STUB = "true" ] ; then
-    if [ -d "/boot/EFI/Boot" ]; then #TODO, make this check case insensative
+    #TODO, make this check case insensative
+    if [ -d "/boot/EFI/Boot" ]; then 
       cp /boot/EFI/Boot /boot/EFI/Boot.bak
     fi
     cp -a /boot/EFI/arch_grub/grubx64.efi  /boot/EFI/Boot/BOOTX64.EFI
